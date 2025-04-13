8ace77f79e995d093555341356eb9bbc798961d0
Vinzlenz82 <143135982+Vinzlenz82@users.noreply.github.com>
2025-04-13 20:28:30 +0200

    Create page.tsx

---
+++ app/my-product/page.tsx
@@ -0,0 +1,9 @@
+// app/my-product/page.tsx
+export default function MyProductPage() {
+  return (
+    <div>
+      <h1>Welcome to My Product!</h1>
+      {/* Add your product content here */}
+    </div>
+  );
+}
